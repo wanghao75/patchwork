b6a2315ea3dd49b1f429ffa8556b9ad2b2301274
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2023-02-27 09:56:37 +0800

    Create patch2pr.py

---
+++ patch2pr/patch2pr.py
@@ -0,0 +1,289 @@
+import time
+import requests
+import os
+
+BRANCHES_MAP = {
+    "master": "master",
+    "openEuler-5.10-LTS-SP1": "OLK-5.10",
+    "openEuler-1.0-LTS": "openEuler-1.0-LTS",
+}
+
+
+def get_mail_step():
+    if os.path.exists("/home/project_series.txt"):
+        os.remove("/home/project_series.txt")
+    r = os.popen('su - www-data -c "getmail --getmaildir="/home/patchwork/" --idle INBOX"').readlines()
+
+
+def download_patches_by_using_git_pw(ser_id):
+    os.popen("rm -f /home/patches/*")
+    if not os.path.exists("/home/patches/{}".format(ser_id)):
+        os.popen("mkdir -p /home/patches/{}".format(ser_id))
+    res = os.popen("git-pw series download {} /home/patches/{}/".format(ser_id, ser_id)).readlines()
+    for r in res:
+        if "error" in r:
+            r2 = os.popen("git-pw series download {} /home/patches/{}/".format(ser_id, ser_id)).readlines()
+
+
+def get_project_and_series_information():
+    if not os.path.exists("/home/project_series.txt"):
+        return
+    with open("/home/project_series.txt", "r", encoding="utf-8") as f:
+        infor = f.readlines()
+
+    return infor
+
+
+def config_get_mail(u_name, u_pass, email_server, path_of_sh):
+    if os.path.exists("/home/patchwork/getmailrc"):
+        return
+
+    retriever = ["[retriever]", "type = SimplePOP3Retriever",
+                 "server = {}".format(email_server), "username = {}".format(u_name), "password = {}".format(u_pass),
+                 "mailboxes = ALL"
+                 ]
+
+    destination = ["[destination]", "type = MDA_external", "path = {}".format(path_of_sh), "ignore_stderr = true"]
+
+    options = ["[options]", "delete = false", "message_log = /home/patchwork/getmail.log",
+               "message_log_verbose = true", "read_all = false", "received = false", "delivered_to = false"]
+
+    with open("/home/patchwork/getmailrc", "a", encoding="utf-8") as f:
+        f.writelines([r + "\n" for r in retriever])
+        f.writelines([r + "\n" for r in destination])
+        f.writelines([r + "\n" for r in options])
+
+
+def config_git_pw(project_name, server_link, token):
+    os.popen("git config pw.server {};git config pw.token {};git config pw.project {}"
+             .format(server_link, token, project_name))
+
+
+# if use the patchwork, we can make it by the following codes
+# use patches
+def make_branch_and_apply_patch(user, token, origin_branch, ser_id):
+    if not os.path.exists("/home/kernel"):
+        os.chdir("/home")
+        os.popen("git clone https://{}:{}@gitee.com/wanghaosq/kernel.git".format(user, token)).readlines()
+        os.chdir("/home/kernel")
+    else:
+        os.chdir("/home/kernel")
+        os.popen("git pull").readlines()
+        time.sleep(30)
+
+    # delete all branches startswith patch
+    # branches_list = os.popen("git branch").readlines()
+    # for b in branches_list:
+    #     if b.startswith("patch-"):
+    #         os.popen("git branch -D %s" % b).readlines()
+
+    new_branch = "patch-%s" % int(time.time())
+    os.popen("git checkout -b %s origin/%s" % (new_branch, origin_branch)).readlines()
+
+    # git am
+    patches_dir = "/home/patches/{}/".format(ser_id)
+    am_res = os.popen("git am --abort;git am %s*.patch" % patches_dir).readlines()
+    am_success = False
+    for am_r in am_res:
+        if am_r.__contains__("Patch failed at"):
+            am_success = False
+            break
+        else:
+            am_success = True
+
+    if am_success:
+        os.popen("git push origin %s" % new_branch).readlines()
+        return new_branch
+
+
+# summit a pr
+def make_pr_to_summit_commit(source_branch, base_branch, token, pr_url_in_email_list, cover_letter, receiver_email):
+    title = "create pr from patch"
+    if pr_url_in_email_list or cover_letter:
+        body = "PR sync from: {} \n{}".format(pr_url_in_email_list, cover_letter)
+    else:
+        body = ""
+
+    data = {
+        "access_token": token,
+        "head": source_branch,
+        "base": base_branch,
+        "title": title,
+        "body": body,
+        "prune_source_branch": "true"
+    }
+    res = requests.post(url="https://gitee.com/api/v5/repos/wanghaosq/kernel/pulls", data=data)
+
+    if res.status_code == 201:
+        pull_link = res.json().get("url")
+        send_mail_to_notice_developers(pull_link, receiver_email)
+
+
+# use email to notice that pr has been created
+def send_mail_to_notice_developers(pr, email_address):
+    import smtplib
+    from email.mime.text import MIMEText
+
+    mail_host = os.getenv("SEND_EMAIL_HOST", "")
+    mail_user = os.getenv("SEND_EMAIL_HOST_USER", "")
+    mail_pass = os.getenv("SEND_EMAIL_HOST_PASSWORD", "")
+    sender = mail_user
+    receivers = ",".join(email_address)
+
+    content = "pull request link: %s" % pr
+    title = "notice"
+    message = MIMEText(content, 'plain', 'utf-8')
+    message['From'] = "patchwork bot <{}>".format(sender)
+    message['To'] = receivers
+    message['Subject'] = title
+
+    try:
+        smtpObj = smtplib.SMTP(mail_host, os.getenv("SEND_EMAIL_PORT", 25))
+        smtpObj.ehlo()
+        smtpObj.starttls()
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers.split(","), message.as_string())
+        smtpObj.quit()
+    except smtplib.SMTPException as e:
+        import logging
+        logging.info("send mail failed, ", e)
+
+
+def get_email_content_sender_and_covert_to_pr_body(ser_id):
+    import psycopg2
+    user = os.getenv("DATABASE_USER")
+    name = os.getenv("DATABASE_NAME")
+    password = os.getenv("DATABASE_PASSWORD")
+    host = os.getenv("DATABASE_HOST")
+
+    conn = psycopg2.connect(database=name, user=user, password=password, host=host, port="5432")
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * from patchwork_series where id={}".format(ser_id))
+    series_rows = cur.fetchall()
+    cover_letter_id = 0
+    for row in series_rows:
+        cover_letter_id = row[-1]
+
+    # no cover
+    patch_sender_email = ""
+    body = ""
+    email_list_link_of_patch = ""
+
+    if cover_letter_id is None or cover_letter_id == 0:
+        cur.execute("SELECT name from patchwork_patch where series_id={}".format(ser_id))
+        patches_names_rows = cur.fetchall()
+        first_path_mail_name = ""
+        if len(patches_names_rows) == 1:
+            first_path_mail_name = patches_names_rows[0][0]
+        else:
+            for row in patches_names_rows:
+                if row[0].__contains__("01/") or row[0].__contains__("1/"):
+                    first_path_mail_name = row[0]
+
+        cur.execute("SELECT headers from patchwork_patch where series_id={} and name='{}'".format(ser_id, first_path_mail_name))
+        patches_headers_rows = cur.fetchall()
+        for row in patches_headers_rows:
+            for string in row[0].split("\n"):
+                who_is_email_list = ""
+                if string.startswith("To: "):
+                    if "<" in string:
+                        who_is_email_list = string.split("<")[1].split(">")[0]
+                    else:
+                        who_is_email_list = string.split(" ")[1]
+                if string.startswith("From: "):
+                    patch_sender_email = string.split("<")[1].split(">")[0]
+                if string.__contains__("https://mailweb.openeuler.org/hyperkitty/list/%s/message/"% who_is_email_list):
+                    email_list_link_of_patch = string.replace("<", "").replace(">", "").replace("message", "thread")
+
+        return patch_sender_email, body, email_list_link_of_patch
+
+    cur.execute("SELECT * from patchwork_cover where id={}".format(cover_letter_id))
+    cover_rows = cur.fetchall()
+    cover_headers = ""
+    cover_content = ""
+    for row in cover_rows:
+        cover_headers = row[3]
+        cover_content = row[4]
+
+    if cover_content == "" or cover_headers == "":
+        return "", "", ""
+
+    cover_who_is_email_list = ""
+    for ch in cover_headers.split("\n"):
+        if ch.startswith("To: "):
+            if "<" in ch:
+                cover_who_is_email_list = ch.split("<")[1].split(">")[0]
+            else:
+                cover_who_is_email_list = ch.split(" ")[1]
+        if ch.__contains__("https://mailweb.openeuler.org/hyperkitty/list/%s/message/" % cover_who_is_email_list):
+            email_list_link_of_patch = ch.replace("<", "").replace(">", "").replace("message", "thread")
+        if ch.startswith("From: "):
+            patch_sender_email = ch.split("From: ")[1].split("<")[1].split(">")[0]
+
+    for ct in cover_content.split("\n"):
+        if ct.__contains__("(+)") or ct.__contains__("(-)") or "mode" in ct or "| " in ct:
+            continue
+        else:
+            body += ct + "\n"
+
+    return patch_sender_email, body, email_list_link_of_patch
+
+
+def main():
+    server = os.getenv("PATCHWORK_SERVER", "")
+    server_token = os.getenv("PATCHWORK_TOKEN", "")
+    repo_user = os.getenv("REPO_OWNER", "")
+    gitee_token = os.getenv("GITEE_TOKEN", "")
+    not_cibot_gitee_token = os.getenv("GITEE_TOKEN_NOT_CI_BOT", "")
+    user_email = os.getenv("EMAIL_HOST_USER", "")
+    user_pass = os.getenv("EMAIL_HOST_PASSWORD", "")
+    mail_server = os.getenv("EMAIL_HOST", "")
+
+    if server == "" or server_token == "" or repo_user == "" or gitee_token == "" or not_cibot_gitee_token == "" or \
+            user_email == "" or user_pass == "" or mail_server == "":
+        print("args can not be empty")
+        return
+
+    # config get-mail tools
+    config_get_mail(user_email, user_pass, mail_server, "/home/patchwork/patchwork/patchwork/bin/parsemail.sh")
+
+    # get mail from email address
+    get_mail_step()
+
+    information = get_project_and_series_information()
+    if len(information) == 0:
+        print("not new series of patches which received by get-mail tool")
+        return
+
+    for i in information:
+        project_name = i.split(":")[0]
+        series_id = i.split(":")[1]
+
+        tag = i.split(":")[2].split("[")[1].split("]")[0]
+        if tag.__contains__(","):
+            branch = tag.split(",")[0]
+        else:
+            branch = tag
+
+        config_git_pw(project_name, server, server_token)
+
+        # download series of patches by series_id
+        download_patches_by_using_git_pw(series_id)
+
+        # get sender email and cover-letter-body
+        sender_email, letter_body, sync_pr = get_email_content_sender_and_covert_to_pr_body(series_id)
+
+        emails_to_notify = [sender_email]
+
+        # use patches
+        source_branch = make_branch_and_apply_patch(repo_user, gitee_token, BRANCHES_MAP[branch], series_id)
+
+        # make pr
+        make_pr_to_summit_commit(source_branch, BRANCHES_MAP[branch], not_cibot_gitee_token,
+                                 sync_pr, letter_body, emails_to_notify)
+
+
+if __name__ == '__main__':
+    main()
