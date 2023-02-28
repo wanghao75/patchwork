efdb0a1c5770d420fae702d08bdd0c018c8b8d0f
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2023-02-28 15:38:06 +0800

    Update production.py

--- patchwork/settings/production.py
+++ patchwork/settings/production.py
@@ -52,7 +52,7 @@ ADMINS = (
 # https://docs.djangoproject.com/en/2.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 #
 
-STATIC_ROOT = os.environ.get('STATIC_ROOT', '/srv/patchwork/htdocs/static')
+STATIC_ROOT = os.environ.get('STATIC_ROOT', 'static')
 
 STATICFILES_STORAGE = (
     'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
