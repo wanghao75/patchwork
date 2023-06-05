c075228144db6ab677a582bae4b632163e3733f8
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2023-06-05 10:21:16 +0800

    match different situation

--- patchwork/parser.py
+++ patchwork/parser.py
@@ -171,10 +171,13 @@ def find_project_by_id_and_subject(list_id, subject):
     subject_x = subject.split("]")[0].split("[")[1]
     if subject_x.count(" ") >= 2:
         v = re.compile(r'v\d+')
+        # match situation like this [PATCH master ...] or [PATCH master v2 ...] or [PATCH v2 master ...]
         if v.match(subject_x.split(" ")[1]):
+            subject_x = subject_x.split(" ")[2]
+        elif v.match(subject_x.split(" ")[2]):
+            subject_x = subject_x.split(" ")[1]
+        else:
             return default
-        # match situation like this [PATCH master 1/3] or [XXX PATCH master 1/3] or [PATCH XXX master 1/3]
-        subject_x = subject_x.split(" ")[1]
     else:
         # match [PATCH master]
         subject_x = subject_x.split(" ")[-1]
